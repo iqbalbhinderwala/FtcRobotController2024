b7b5d1ef0fd117730ff9002eed2533a8ef7908c3
Alex Balan <alexandru.balan@gmail.com>
2024-11-02 21:29:37 -0700

    Add templates

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateExternalClass.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.shortcircuit;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class TemplateExternalClass {
+    private LinearOpMode opMode;   // gain access to methods in the calling OpMode.
+
+    /* Declare Component members. */
+
+    public TemplateExternalClass(LinearOpMode myOpMode) {
+        opMode = myOpMode;
+    }
+
+    // Initialize all hardware components.
+    public void init() {
+    }
+
+    // Start all hardware components.
+    public void start(){
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateTeleOp.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.shortcircuit;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="Template Tele Op", group="Template")
+@Disabled
+public class TemplateTeleOp extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    TemplateExternalClass hardwareComponent = new TemplateExternalClass(this);
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize components.
+        hardwareComponent.init();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // Start components.
+        hardwareComponent.start();
+
+        // Run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Show the elapsed game time.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
