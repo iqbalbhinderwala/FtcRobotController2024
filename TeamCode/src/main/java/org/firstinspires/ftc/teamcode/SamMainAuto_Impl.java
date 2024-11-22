72d5d9c3053d62f88641b804e4eca4642b6dca17
Alex Balan <alexandru.balan@gmail.com>
2024-11-21 16:20:15 -0800

    Iterate on auto plan.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SamMainAuto_Impl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SamMainAuto_Impl.java
@@ -25,25 +25,32 @@ public class SamMainAuto_Impl {
         joints = new SamJoints(opMode);
         claw = new SamClaw(opMode);
 
+        // Initialize components.
+        nav.init();
+        joints.init();
+        claw.init();
+
         // Send telemetry message to signify robot waiting
         opMode.telemetry.addData("Status", "Ready to start.");
+
+        joints.tryResetEncoders();
+        if (joints.isFullyCalibrated()) {
+            opMode.telemetry.addData("", "*** FULLY CALIBRATED ***");
+        } else {
+            opMode.telemetry.addData("", "WARNING: !!! NOT CALIBRATED !!!");
+        }
         opMode.telemetry.update();
 
         // Wait for driver to press PLAY before robot allowed to move.
         opMode.waitForStart();
         runtime.reset();
 
-        // Initialize components.
-        nav.init();
-        joints.init();
-        claw.init();
-
         // Start components.
         joints.start();
 
         // ------ Start performing tasks ------
 
-        // High clip bar is in front: 16in +/- 4in
+        // High clip bar is in front: 23in +/- 4in
         //   Split the drive in 2 segments:
         //      Close the claw so we don't drop the pre-loaded specimen
         //      Activate HIGHBAR joints preset
@@ -65,7 +72,7 @@ public class SamMainAuto_Impl {
         // Drive forward 12 inches at high power (blocking)
         //   (high clip bar is in front: 16in +/- 4in)
         if (opMode.opModeIsActive()) {
-            nav.driveDistance(16 - 4, 0, 1);
+            nav.driveDistance(23 - 5, 0, 1);
         }
 
         // Wait for the HIGHBAR preset to complete
@@ -75,7 +82,7 @@ public class SamMainAuto_Impl {
 
         // Perform clipping by driving 8 inches slowly
         if (opMode.opModeIsActive()) {
-            nav.driveDistance(4 * 2, 0, 0.2);
+            nav.driveDistance(5 + 3, 0, 0.2);
         }
 
         // Open the claw
@@ -85,7 +92,7 @@ public class SamMainAuto_Impl {
 
         // Drive back 16 inches at high power (blocking)
         if (opMode.opModeIsActive()) {
-            nav.driveDistance(-16, 0, 1);
+            nav.driveDistance(-6, 0, 1);
         }
 
         // Initiate PARKED joints preset (non-blocking)
@@ -93,21 +100,52 @@ public class SamMainAuto_Impl {
             joints.activatePreset(SamJoints.Pose.PARKED);
         }
 
-        // Strafe to parking zone (blocking)
+        opMode.sleep(500);
+        // Strafe right to parking zone (blocking)
+        double DISTANCE_PARKING_TO_HIGHBAR = 32;
         if (opMode.opModeIsActive()) {
-            double DISTANCE_TO_PARK_ZONE_INCHES = 16;
-            if (startSide == Alliance.Side.RIGHT) {
-                DISTANCE_TO_PARK_ZONE_INCHES *= -1;
-            }
-
-            // Slow down last quarter
-            nav.driveDistance(0, 0.75 * DISTANCE_TO_PARK_ZONE_INCHES, 1);
-            nav.driveDistance(0, 0.25 * DISTANCE_TO_PARK_ZONE_INCHES, 0.3);
+            nav.driveDistance(0, -DISTANCE_PARKING_TO_HIGHBAR, 1);
         }
 
         // Wait for PARKED preset to complete
         while(opMode.opModeIsActive() && joints.isPresetActive()) {
             joints.stepActivePreset();
         }
+
+        // TUrn from 0 to 90 to 180 degree heading
+        nav.turnToHeading(1, 90);
+        nav.turnToHeading(1, 180);
+
+        joints.activatePreset(SamJoints.Pose.RAIL);
+
+        nav.driveDistance(-3, 0, .3);
+
+        // Wait for PARKED preset to complete
+        while(opMode.opModeIsActive() && joints.isPresetActive()) {
+            joints.stepActivePreset();
+        }
+
+        nav.driveDistance(5, 0, .2);
+        opMode.sleep(500);
+        claw.closed();
+        opMode.sleep(500);
+        joints.activatePreset(SamJoints.Pose.RAIL_UP);
+        opMode.sleep(100);
+        nav.driveDistance(-5, 0, 0.2);
+
+        // Turn from 180 to 90 to 0 heading
+        nav.turnToHeading(1, 90);
+        nav.turnToHeading(1, 0);
+
+        joints.activatePreset(SamJoints.Pose.HIGHBAR);
+        // Wait for HIGHBAR preset to complete
+        while(opMode.opModeIsActive() && joints.isPresetActive()) {
+            joints.stepActivePreset();
+        }
+
+        nav.driveDistance(-12, 0, 1);
+
+        nav.driveDistance(0, DISTANCE_PARKING_TO_HIGHBAR, 1);
+
     }
 }
