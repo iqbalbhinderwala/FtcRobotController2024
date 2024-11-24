6e38c59006b84b0d1ac7d6a9dea4eb111258b2d7
Alex Balan <alexandru.balan@gmail.com>
2024-11-23 16:31:33 -0800

    Increase MAIN teleop speed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SamMainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SamMainTeleop.java
@@ -16,9 +16,9 @@ public class SamMainTeleop extends LinearOpMode {
     public SamJoints joints = null;
     public SamClaw claw = null;
 
-    double MAX_DRIVE_SPEED  = 0.7;
-    double MAX_STRAFE_SPEED = 0.7;
-    double MAX_TURN_SPEED   = 0.7;
+    double MAX_DRIVE_SPEED  = 0.8;
+    double MAX_STRAFE_SPEED = 0.8;
+    double MAX_TURN_SPEED   = 0.8;
 
     ElapsedTime lastPress = new ElapsedTime();
     ElapsedTime lastLeftBumper = new ElapsedTime();
