75fb979969c242f7f6e7a0206574d61de18def2d
Alex Balan <alexandru.balan@gmail.com>
2025-10-17 00:32:09 -0700

    Add team custom calibration for Logitech C270 Webcam. This is necessary since the default default focal distance in the SDK is off by 15%.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera/python/WebcamCalib.py
@@ -0,0 +1,100 @@
+# From: https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
+
+# pip install -r requirements.txt
+
+import numpy as np
+import cv2 as cv
+import glob
+
+# Grid size (number of corners)
+NC = 8
+NR = 6
+
+# Termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# Prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((NR*NC,3), np.float32)
+objp[:,:2] = np.mgrid[0:NC,0:NR].T.reshape(-1,2)
+ 
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+ 
+images = glob.glob('C:/ftc/LogitechC270/*.png')
+ 
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+ 
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (NC,NR), None)
+ 
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+ 
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+ 
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (NC,NR), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(100)
+ 
+cv.destroyAllWindows()
+
+#     distortionCoefficients (an 8-element float array): distortion coefficients in the following form
+#         (r:radial, t:tangential): [r0, r1, t0, t1, r2, r3, r4, r5]
+#         see https://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html
+
+# Calibration
+flags = None
+flags = cv.CALIB_ZERO_TANGENT_DIST # | cv.CALIB_FIX_ASPECT_RATIO | cv.CALIB_RATIONAL_MODEL
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(
+    objpoints, imgpoints, gray.shape[::-1], None, None, flags=flags
+)
+
+# Calculate reprojection error
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
+
+print("Total error: {}".format(mean_error/len(objpoints)) )
+print("Camera matrix :")
+print(mtx)
+print("Distortion Coefficients:")
+print(dist.flatten())
+
+dist8 = np.zeros((8, 1))
+flat_dist = dist.flatten()
+
+# Fill dist8 with available values, pad remainder with zeros
+for i in range(min(8, len(flat_dist))):
+    dist8[i, 0] = flat_dist[i]
+
+
+print('Add the following to TeamCode/src/main/res/xml/teamwebcamcalibrations.xml:')
+print(' ')
+
+print('<!-- ======================================================================================= -->')
+print('')
+print('<!-- Logitech HD Webcam C270 Calibration - By ShortCircuit 2025 - Via OpenCV -->')
+print('<Camera vid="Logitech" pid="0x0825">')
+print('    <Calibration')
+print('        size="{} {}"'.format(gray.shape[1], gray.shape[0]))
+print('        focalLength="{:.3f}f, {:.3f}f"'.format(mtx[0,0], mtx[1,1]))
+print('        principalPoint="{:.3f}f, {:.3f}f"'.format(mtx[0,2], mtx[1,2]))
+print('        distortionCoefficients="{}"'.format(', '.join(['{:.6g}'.format(coef) for coef in dist8.flatten()])))
+print('        />')
+print('</Camera>')
+print('')
+print('<!-- ======================================================================================= -->')
+
+print(' ')
+print('Alternatively, add the following to AprilTagProcessor.Builder(). (see ConceptAprilTag.java):')
+print('#'*60)
+print("     .setLensIntrinsics({:.3f}, {:.3f}, {:.3f}, {:.3f})".format(mtx[0,0], mtx[1,1], mtx[0,2], mtx[1,2]))
+print('#'*60)
